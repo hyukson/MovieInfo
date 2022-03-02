ca24b48d6f2f78a0b749879dc7533ee8e30595c1
hyukson <93583305+hyukson@users.noreply.github.com>
2022-03-02 19:40:09 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org", "themoviedb.org"],
   },
+  env: {
+    NEXT_PUBLIC_MOVIE_KEY: process.env.NEXT_PUBLIC_MOVIE_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_IMG_URL: process.env.NEXT_PUBLIC_IMG_URL,
+  },
 };
 
 module.exports = nextConfig;
